6942b71df4f8e1d5bffb86b23ec779e16e8db844
Welling Guzman <hola@wellingguzman.com>
2018-04-21 23:32:22 -0400

    set the correct port for app image

--- bin/run_container.sh
+++ bin/run_container.sh
@@ -8,4 +8,4 @@ docker run -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=directus -e MYSQL_U
 docker run --link mysql:mysql -p 8080:8080 --name directus-api -d getdirectus/api:2.0.0-alpha.1
 
 # Start directus app
-docker run -p 8000:8080 --name directus-app -d getdirectus/app:7.0.0-alpha.1
+docker run -p 8000:8888 --name directus-app -d getdirectus/app:7.0.0-alpha.1
